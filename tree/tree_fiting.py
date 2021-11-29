d246a6064ff7dfc9219ae0c8a01e28b63b128b08
Johannes Muenichsdorfer <jomapp.developer@gmail.com>
2021-11-29 15:16:59 +0100

    Start implementing best candidate split

--- tree/tree_fiting.py
+++ tree/tree_fiting.py
@@ -78,23 +78,33 @@ def generate_root(tree):
     root = tree.create_node(random_x1_pixel_index_root, random_x2_pixel_index_root, random_threshold_root)
     return root
 
-def select_best_candidate_split_for_node():
+def select_best_candidate_split_for_node(I, residual_image_vector, candidate_splits, Q): # I is [0, 255, 244, 123] grayscale values for 400 extracted pixels
+    x1, x2, threshold = candidate_splits[0]
+    Q_0_l = []
+    Q_0_r = []
+    for index in Q:
+        if np.abs(I[index][x1] - I[index][x2]) > threshold:
+            Q_0_l.append(index)
+        else:
+            Q_0_r.append(index)
+    mu_0_l = 1 / len(Q_0_l) * np.sum(residual_image_vector[Q_0_l]]) 
+    mu_0_r = 1 / len(Q_0_r) * np.sum()
     return
 
-# def generate_candidate_splits(number_candidate_splits=20):
-#     candidate_splits = []
-#     for i in range(0,number_candidate_splits-1): 
-#         random_x1_pixel_index = np.random.randint(0, number_selected_features)
-#         random_x2_pixel_index = np.random.randint(0, number_selected_features)
-#         while (random_x1_pixel_index == random_x2_pixel_index):
-#             random_x2_pixel_index = np.random.randint(0, number_selected_features)
-#         # generating 20 different x1, x2, threshold triplets and calcualting resiudual error here!
+def generate_candidate_splits(number_candidate_splits=20):
+    candidate_splits = []
+    for i in range(0,number_candidate_splits-1): 
+        random_x1_pixel_index = np.random.randint(0, number_selected_features)
+        random_x2_pixel_index = np.random.randint(0, number_selected_features)
+        while (random_x1_pixel_index == random_x2_pixel_index):
+            random_x2_pixel_index = np.random.randint(0, number_selected_features)
+        # generating 20 different x1, x2, threshold triplets and calcualting resiudual error here!
 
-#         random_threshold = np.random.randint(0, 255) # we take absolute value for node split pixel differnce
-#         candidate_splits.append(zip(random_x1_pixel_index, random_x2_pixel_index, random_threshold))
-#     return candidate_splits
+        random_threshold = np.random.randint(0, 255) # we take absolute value for node split pixel differnce
+        candidate_splits.append(zip(random_x1_pixel_index, random_x2_pixel_index, random_threshold))
+    return candidate_splits
 
-def generate_candidate_splits(current_node_id, current_depth, max_depth):  
+def generate_complete_node(current_node_id, current_depth, max_depth):  
     if current_depth == max_depth:
         return
         
@@ -113,11 +123,15 @@ def generate_candidate_splits(current_node_id, current_depth, max_depth):
     left = tree.create_node(random_x1_pixel_index_left_child, random_x2_pixel_index_left_child, random_threshold_left_child, parent_id=current_node_id)  # node left, has parent current_node
     right = tree.create_node(random_x1_pixel_index_right_child, random_x2_pixel_index_right_child, random_threshold_right_child, parent_id=current_node_id)  # node right, has parent current_node
 
-    return (generate_candidate_splits(left.id, current_depth+1, max_depth), generate_candidate_splits(right.id, current_depth+1, max_depth))
+    return (generate_complete_node(left.id, current_depth+1, max_depth), generate_complete_node(right.id, current_depth+1, max_depth))
 
 tree = Tree()
 root = generate_root(tree)
-generate_candidate_splits(current_node_id=root.id, current_depth=0, max_depth=2)
+generate_complete_node(current_node_id=root.id, current_depth=0, max_depth=2)
 
-print(len(tree.nodes))
-print(tree.print_tree())
+# print(len(tree.nodes))
+# print(tree.print_tree())
+
+index = [0,2]
+array = np.array(["TRest", "tste", "no", "no", "no"])
+print(array[index])
