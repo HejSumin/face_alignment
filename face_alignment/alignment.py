55eebfd5a2a0e1844ab33041c6b5166699500b8c
RasmusCarl <rroenneberg@gmail.com>
2021-12-08 20:34:03 +0100

    optimize eq8

--- face_alignment/alignment.py
+++ face_alignment/alignment.py
@@ -1,10 +1,34 @@
 from matplotlib.patches import Circle
 import numpy as np
-
+from sklearn.metrics.pairwise import euclidean_distances
+import scipy.optimize as opt
+import math
 from face_detection.face_detection import get_circle_bounding_box_for_image
 
 
+def rotate_matrix(theta):
+    T = np.zeros((2,2))
+    T[0][0] = math.cos(theta) 
+    T[0][1] = -math.sin(theta)
+    T[1][0] = math.sin(theta)
+    T[1][1] = math.cos(theta)
+    return T
 
+def equation_8(params, x_bar, x):
+    scale = params[0]
+    theta = params[1]
+    R     = rotate_matrix(theta) 
+    return np.sum(euclidean_distances(x_bar - np.dot(x*scale, R)))
+
+
+def align_shapes(shape1, shape2):
+    mean  = np.mean(shape2, axis=0)
+    align = shape1 - mean
+    return align
+    
+def optimize_equation_8(x_bar, x):
+    res  = opt.fmin(func=equation_8, x0=[0,0], args=(x_bar, x))
+    return res
 
 
 def get_bounding_coords(image, circle):
