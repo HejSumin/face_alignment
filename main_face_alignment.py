87b0676c718cd0f40e6f11709723b743fa49be18
Johannes Muenichsdorfer <jomapp.developer@gmail.com>
2022-01-04 00:25:00 +0100

    Prediction changes and implement S_true scaling function

--- main_face_alignment.py
+++ main_face_alignment.py
@@ -20,21 +20,18 @@ annotations_path = 'data/annotation/'
 
 print("... starting training ...")
 print("... creating training data ... ")
-
 training_data = fa.create_training_data(data_path + "train_1/", annotations_path)
 np.save("np_data/run_input_training_data", training_data)
 
 print("... loading training data ... ")
 training_data = np.load("np_data/run_input_training_data.npy", allow_pickle=True)
 
-#Not working, index out of bound when loading new intensities
 print("... starting training trees in cascade ...")
 start = timer()
 training_data_result, model = fa.train_multiple_cascades(training_data, regression_tree_max_depth=5, use_exponential_prior=True)
 end = timer()
 
 pickle.dump(model, open("run_output/run_output_model.p", "wb"))
-
 np.save("run_output/run_output_training_data_result", training_data_result)
 
 print("Run finished in: (Time)", timedelta(seconds=end-start))
